1e16571c47378fef96e87b715090fe1ae03f9b6d
Cariad Eccleston <cariad@cariad.io>
2021-05-17 10:17:36 +0000

    WIP

--- deepracer/reward.py
+++ deepracer/reward.py
@@ -3,11 +3,7 @@ from typing import Any, Dict
 
 def reward_function(params: Dict[str, Any]):
 
-    if float(params["distance_from_center"]) <= float(params["track_width"]) * 0.6:
-        return 2.0
-
-    elif bool(params["all_wheels_on_track"]):
-        return 0.2
-
-    else:
+    if not bool(params["all_wheels_on_track"]):
         return 1e-3
+
+    return float(params["track_width"]) - float(params["distance_from_center"]) * 2
