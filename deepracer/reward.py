6d653b473715a060f959466cad61346ef3717f6c
Cariad Eccleston <cariad@cariad.io>
2021-05-17 11:15:55 +0000

    WIP

--- deepracer/reward.py
+++ deepracer/reward.py
@@ -2,8 +2,27 @@ from typing import Any, Dict
 
 
 def reward_function(params: Dict[str, Any]):
+    """
+    Example of rewarding the agent to follow center line
+    """
 
-    if not bool(params["all_wheels_on_track"]):
-        return 1e-3
+    # Read input parameters
+    track_width = params["track_width"]
+    distance_from_center = params["distance_from_center"]
 
-    return float(params["track_width"]) - float(params["distance_from_center"]) * 2
+    # Calculate 3 markers that are at varying distances away from the center line
+    marker_1 = 0.1 * track_width
+    marker_2 = 0.25 * track_width
+    marker_3 = 0.5 * track_width
+
+    # Give higher reward if the car is closer to center line and vice versa
+    if distance_from_center <= marker_1:
+        reward = 1.0
+    elif distance_from_center <= marker_2:
+        reward = 0.5
+    elif distance_from_center <= marker_3:
+        reward = 0.1
+    else:
+        reward = 1e-3  # likely crashed/ close to off track
+
+    return float(reward)
